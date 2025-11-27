a3678c5d12142bc8ee48a4a4df9dc137e18d816e
pstol123 <p.stolarczy3@students.uw.edu.pl>
2025-11-27 23:23:17 +0100

    movement_x

--- src/game/player.rs
+++ src/game/player.rs
@@ -23,9 +23,29 @@ pub fn spawn_player(mut commands: Commands) {
     commands.spawn((
         Sprite::from_color(
             Color::srgb(0.3, 0.0, 0.6),
-            Vec2::new(800.0, 50.0),
+            Vec2::new(50.0, 50.0),
         ),
         Transform::from_xyz(0.0, 0.0, 0.0),
-        Player::new(800.0, 50.0, 20.0),
+        Player::new(50.0, 50.0, 50.0),
     ));
-}czy
+}
+
+pub fn player_movement(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&mut Transform, &mut Player)>,
+    time: Res<Time>,
+) {
+    for (mut transform, mut player) in query.iter_mut(){
+        let mut movementx = 0.0;
+        if keyboard_input.pressed(KeyCode::KeyD){
+            movementx = time.delta_secs() 
+        }
+
+        if keyboard_input.pressed(KeyCode::KeyA){
+            movementx = time.delta_secs() * -1.0;
+        }
+
+        player.pos.x += movementx * player.speed;
+        transform.translation.x += movementx * player.speed;
+    }
+}
