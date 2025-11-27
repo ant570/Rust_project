24eb3adaf12e3542da3f80691d7bd354a2ecbd1d
pstol123 <p.stolarczy3@students.uw.edu.pl>
2025-11-27 23:36:44 +0100

    jump & gravity

--- src/game/player.rs
+++ src/game/player.rs
@@ -4,15 +4,19 @@ use crate::game::position::Position2;
 #[derive(Component)]
 pub struct Player{
     pub pos: Position2,
-    pub speed: f32,
+    pub speed_x: f32,
+    pub jump_speed: f32,
+    pub gravity: f32,
 }
 
 
 impl Player{
-    pub fn new(x: f32, y: f32, speed: f32) -> Self {
+    pub fn new(x: f32, y: f32, speed_x: f32, jump_speed: f32, gravity: f32) -> Self {
         Player{
             pos : Position2::new(x, y),
-            speed,
+            speed_x,
+            jump_speed,
+            gravity,
         }
     }
 }
@@ -26,7 +30,7 @@ pub fn spawn_player(mut commands: Commands) {
             Vec2::new(50.0, 50.0),
         ),
         Transform::from_xyz(0.0, 0.0, 0.0),
-        Player::new(50.0, 50.0, 50.0),
+        Player::new(50.0, 50.0, 50.0, 100.0, 1.0),
     ));
 }
 
@@ -36,16 +40,26 @@ pub fn player_movement(
     time: Res<Time>,
 ) {
     for (mut transform, mut player) in query.iter_mut(){
-        let mut movementx = 0.0;
+        let mut movement_x = 0.0;
+        let mut movement_y = 0.0;
+
+        //Movement x
         if keyboard_input.pressed(KeyCode::KeyD){
-            movementx = time.delta_secs() 
+            movement_x = time.delta_secs() 
         }
-
         if keyboard_input.pressed(KeyCode::KeyA){
-            movementx = time.delta_secs() * -1.0;
+            movement_x = time.delta_secs() * -1.0;
         }
+        player.pos.x += movement_x * player.speed_x;
+        transform.translation.x += movement_x * player.speed_x;
 
-        player.pos.x += movementx * player.speed;
-        transform.translation.x += movementx * player.speed;
+        //Movement y
+        if keyboard_input.pressed(KeyCode::Space) || keyboard_input.pressed(KeyCode::KeyW){
+            movement_y = time.delta_secs() * player.jump_speed;
+        }
+        movement_y -= player.gravity;
+        player.pos.y += movement_y;
+        transform.translation.y += movement_y;
+        
     }
 }
